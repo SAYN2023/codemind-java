28a9e4e0536f6a5d6bcb17b813610718c655e550
Hi, I'm Sayantan <151954905+SAYN2023@users.noreply.github.com>
2024-07-31 16:08:29 +0530

    import java.util.Scanner; public class sayantan{     public static void main(String[] args){         int r, sum;         Scanner read=new Scanner(System.in);         r=read.nextInt();         sum=(r*(r+1))/2;         System.out.println(sum);
        }
    }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sayantan{
+    public static void main(String[] args){
+        int r, sum;
+        Scanner read=new Scanner(System.in);
+        r=read.nextInt();
+        sum=(r*(r+1))/2;
+        System.out.println(sum);
+       
+    }
+}
